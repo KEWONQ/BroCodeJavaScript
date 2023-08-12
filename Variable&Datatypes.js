6b126f0ec358967990369528b0f062dbe4cc6d6b
Casey Clark <casey522009@gmail.com>
2023-08-12 11:05:02 +0800

    Variables

--- Variable&Datatypes.js
+++ Variable&Datatypes.js
@@ -16,5 +16,5 @@ console.log('You are', age, 'years old');
 console.log('enrolled:', student);
 
 document.getElementById('p1').innerHTML = 'Hello ' + firstName
-document.getElementById('p2').innerHTML = 'You are ' + age + ' years old'
+document.getElementById('p2').innerHTML = 'You are ' + age + ' years old.'
 document.getElementById('p3').innerHTML = 'Enrolled: ' + student
