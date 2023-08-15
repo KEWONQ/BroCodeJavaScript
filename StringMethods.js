a67400983546600668d96ca4db1821790b0b6de1
Casey Clark <casey522009@gmail.com>
2023-08-15 11:08:07 +0800

    StringMethods

---
+++ StringMethods.js
@@ -0,0 +1,16 @@
+// useful string properties & methods
+
+let username = "  Casey Clark  ";
+let phoneNumber = "123-456-7890"
+
+// console.log(username.length);
+// console.log(username.charAt(6));
+// console.log(username.indexOf("C"));
+// console.log(username.lastIndexOf("C"));
+// username = username.trim();
+// username = username.toUpperCase();
+// username = username.toLowerCase();
+
+phoneNumber = phoneNumber.replaceAll("-", "/")
+
+console.log(phoneNumber)
